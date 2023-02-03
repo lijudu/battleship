7a7558a16aaa2b8d2d84ecbbc60300ea295d7c4c
Lisa <mkoliju@gmail.com>
2023-02-02 20:22:41 -0800

    Function isHIt() increments ship.hits

--- src/ship.js
+++ src/ship.js
@@ -1,11 +1,11 @@
 const ship = (length, hits, sunk) => {
-    const isHit = (hits) => (hits++);
+    const isHit = () => (hits + 1);
     const isSunk = (length, hits) => console.log('calculate based on length and hit')
 
     return { length, hits, sunk, isHit, isSunk}
 }
 
-const player1 = ship(2,0,false);
+const player1 = ship(2,5,false);
 
 
 module.exports = player1;
